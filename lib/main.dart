90b344cb982b88cc65cdf4b3869e5f2f1a18a765
shabake <45329453@qq.com>
2020-08-06 17:00:59 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'theme_manager.dart';
+
+void main() => runApp(
+      ChangeNotifierProvider<ThemeManager>(
+        create: (_) {
+          return ThemeManager(ThemeType.light);
+        },
+        child: MyApp(),
+      ),
+    );
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: Provider.of<ThemeManager>(context, listen: true).themeData,
+      home: GHomePage(),
+    );
+  }
+}
+
+class GHomePage extends StatefulWidget {
+  @override
+  _GHomePageState createState() => _GHomePageState();
+}
+
+class _GHomePageState extends State<GHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("GHThemeManager"),
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "点击悬浮button切换主题",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              "${Provider.of<ThemeManager>(context, listen: true).themeType != ThemeType.light ? "默认":"暗黑"}",
+              style: TextStyle(fontSize: 30),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Provider.of<ThemeManager>(context, listen: false).changeTheme();
+          },
+          child: Icon(Icons.home)),
+    );
+  }
+}
---
+++ lib/theme_manager.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+enum ThemeType {
+  light,
+  dark,
+}
+
+class ThemeManager with ChangeNotifier {
+  ThemeData themeData;
+
+  ThemeType themeType;
+
+  ThemeManager(ThemeType type) {
+    themeType = type;
+    if (type == ThemeType.dark) {
+      themeData = ThemeData.light();
+    } else {
+      themeData = ThemeData.dark();
+    }
+  }
+
+  void changeTheme() {
+    if (themeType == ThemeType.light) {
+      _updateTheme(ThemeType.dark);
+    } else {
+      _updateTheme(ThemeType.light);
+    }
+  }
+  void _updateTheme(ThemeType type){
+    themeType = type;
+    if(themeType == ThemeType.dark) {
+      themeData = ThemeData.light();
+    } else {
+      themeData = ThemeData.dark();
+    }
+    notifyListeners();
+  }
+}
+
+
